ced0ee7b20452762ffce63d0b61ebde00ec1a605
Oleksandr Balabanov <balabanovsasha@gmail.com>
2024-01-19 01:27:55 +0100

    Corrected typos for mmlu dataset processing

--- experiments/lora_ensembles/train/lora_ens_train_config_dataclass.py
+++ experiments/lora_ensembles/train/lora_ens_train_config_dataclass.py
@@ -10,15 +10,15 @@ from experiments.lora_ensembles.pretrained_models.pretrained_models_checkpoints 
 @dataclass
 class LoraEnsTrainConfig:
     checkpoint:str=MISTRAL_CHECKPOINT
-    train_dataset:str="commonsense_qa"
+    train_dataset:str="mmlu_stem"
     epochs:int=10
     batch_size:int=8
     learning_rate:float=0.000005
     lora_rank:int=8
     lora_alpha:int=32
-    lora_dropout:float=0.1
-    lora_l2:float=0.1
-    regular_l2:float=0
+    lora_dropout:float=0
+    lora_l2:float=0
+    regular_l2:float=0.01
     max_len_train:int = 128
     max_len_val:int = 128
     target_modules:List[str] = field(default_factory=lambda: ["q_proj", "v_proj"])
