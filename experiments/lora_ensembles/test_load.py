89e2db3e87ac9b8802bde57bc87159779fc2cf00
Hampus Linander <hampus.linander@gu.se>
2024-01-28 23:46:52 +0100

    Add mmlu ss and stem test load

--- experiments/lora_ensembles/test_load.py
+++ experiments/lora_ensembles/test_load.py
@@ -2,26 +2,65 @@
 from lib.distributed_trainer import distributed_train
 from lib.generic_ablation import generic_ablation_configs
 from lib.train_distributed import request_train_runs
-from lib.serialization import deserialize_model, DeserializeConfig
+from lib.ensemble import create_ensemble_config
+from lib.serialization import deserialize_model, DeserializeConfig, is_serialized
 from lib.ddp import ddp_setup
 
-from experiments.lora_ensembles.mmlu_configs import create_mmlu_config
+from experiments.lora_ensembles.train.lora_ens_train_config import (
+    create_lora_ens_train_run_config,
+)
+from experiments.lora_ensembles.train.lora_ens_train_config_dataclass import (
+    LoraEnsTrainConfig,
+)
 
 
 def main():
     print("Start")
     device = ddp_setup()
 
-    configs = generic_ablation_configs(
-        create_mmlu_config,
-        dict(
-            ensemble_id=list(range(5)),
-            num_gpus=[1],
-            dataset=["mmlu_stem", "mmlu_ss"],
+    def create_ensemble(lora_ens_train_config: LoraEnsTrainConfig):
+        def create_config(ensemble_id):
+            return create_lora_ens_train_run_config(ensemble_id, lora_ens_train_config)
+
+        return create_config
+
+    ensemble_stem_config = create_ensemble_config(
+        create_ensemble(
+            LoraEnsTrainConfig(
+                train_dataset="mmlu_stem",
+                learning_rate=1.25e-6,
+                batch_size=2,
+                max_len_train=512,
+                max_len_val=512,
+                epochs=15,
+                lora_l2=10.0,
+                lora_dropout=0.0,
+                regular_l2=0,
+            )
         ),
+        n_members=10,
     )
+    ensemble_ss_config = create_ensemble_config(
+        create_ensemble(
+            LoraEnsTrainConfig(
+                train_dataset="mmlu_ss",
+                learning_rate=1.25e-6,
+                batch_size=2,
+                max_len_train=512,
+                max_len_val=512,
+                epochs=15,
+                lora_l2=10.0,
+                lora_dropout=0.0,
+                regular_l2=0,
+            )
+        ),
+        n_members=10,
+    )
+
+    configs = ensemble_ss_config.members + ensemble_stem_config.members
     for config in configs:
-        deserialize_model(DeserializeConfig(config, device))
+        if not is_serialized(config):
+            print(f"Couldn't load {config}")
 
 
 if __name__ == "__main__":
