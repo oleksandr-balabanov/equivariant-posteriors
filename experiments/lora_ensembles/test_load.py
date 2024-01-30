d1ad0fdb18003b2de888b93bb00ee9077a26bd9a
Hampus Linander <hampus.linander@gu.se>
2024-01-30 22:55:49 +0100

    Add regular l2 ensemble configs to test

--- experiments/lora_ensembles/test_load.py
+++ experiments/lora_ensembles/test_load.py
@@ -57,7 +57,67 @@ def main():
         n_members=10,
     )
 
-    configs = ensemble_ss_config.members + ensemble_stem_config.members
+    ensemble_qs_regular = create_ensemble_config(
+        create_ensemble(
+            LoraEnsTrainConfig(
+                train_dataset="commonsense_qa",
+                epochs=10,
+                batch_size=8,
+                learning_rate=5e-06,
+                lora_rank=8,
+                lora_alpha=32,
+                lora_dropout=0,
+                lora_l2=0,
+                regular_l2=1,
+                max_len_train=128,
+                max_len_val=128,
+            )
+        ),
+        n_members=10,
+    )
+
+    ensemble_ss_reg = create_ensemble_config(
+        create_ensemble(
+            LoraEnsTrainConfig(
+                train_dataset="mmlu_ss",
+                # learning_rate=0.000005 * batch_size / 8,
+                learning_rate=1.25e-6,
+                batch_size=2,
+                max_len_train=512,
+                max_len_val=512,
+                epochs=15,
+                lora_l2=0.0,
+                lora_dropout=0.0,
+                regular_l2=1.0,
+            )
+        ),
+        n_members=10,
+    )
+    ensemble_stem_reg = create_ensemble_config(
+        create_ensemble(
+            LoraEnsTrainConfig(
+                train_dataset="mmlu_stem",
+                # learning_rate=0.000005 * batch_size / 8,
+                learning_rate=1.25e-6,
+                batch_size=2,
+                max_len_train=512,
+                max_len_val=512,
+                epochs=15,
+                lora_l2=0.0,
+                lora_dropout=0.0,
+                regular_l2=1.0,
+            )
+        ),
+        n_members=10,
+    )
+
+    configs = (
+        ensemble_ss_config.members
+        + ensemble_stem_config.members
+        + ensemble_qs_regular.members
+        + ensemble_ss_reg.members
+        + ensemble_stem_reg.members
+    )
     for config in configs:
         if not is_serialized(config):
             print(f"Couldn't load {config}")
