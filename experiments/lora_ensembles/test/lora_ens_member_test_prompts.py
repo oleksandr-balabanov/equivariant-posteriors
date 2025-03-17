df0d8669e161b16e5aa5c3b73860e26543f0049f
Oleksandr Balabanov <balabanovsasha@gmail.com>
2025-03-17 21:17:27 +0100

    iclr2025

--- experiments/lora_ensembles/test/lora_ens_member_test_prompts.py
+++
@@ -1,5 +0,0 @@
-TEST_PROMPTS = [
-    "The color of the sky is blue.",
-    "The square root of 123456789 is 11111.1110606.",
-    "Multimodal language model is  a type of artificial intelligence that processes and understands information from multiple modalities, such as text, images, and possibly sound, enabling more comprehensive and nuanced interactions and analyses."
-]
