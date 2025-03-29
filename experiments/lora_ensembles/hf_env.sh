ff0e447b7ecfb1d8060a8b3b110e7612034ea1c2
oleksandr-balabanov <balabanovsasha@gmail.com>
2025-03-29 15:43:52 +0100

    Update hf_env.sh

--- experiments/lora_ensembles/hf_env.sh
+++ experiments/lora_ensembles/hf_env.sh
@@ -1,4 +1,3 @@
-export ENTVAR= #Set a base directory for storing model files and cache.  
 export TRANSFORMERS_OFFLINE=1
 export TRANSFORMERS_CACHE=$ENTVAR/LLM/huggingface_models
 export SINGULARITYENV_TRANSFORMERS_OFFLINE=1
